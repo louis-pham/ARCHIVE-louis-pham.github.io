b0a2bb3c2e3be5ee689d4f044e4f290512dc62e2
louis-pham <l.c.pham@hotmail.com>
2019-10-11 15:31:39 -0400

    Fixed moon and heatpipe positions; added max-width for skill images; updated readme;

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ The github repository for my personal site. The master branch contains the build
 
 The actual page can be found [here](https://louis-pham.github.io/).
 ## Todo
-- fix scrolling on iOS Safari and Chrome
 - fix SVG flickering in Edge
   - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/10440554/
   - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/16011476/
--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -3,12 +3,10 @@ import React from 'react';
 function Intro(props) {
   return (
     <section className="intro">
-      <div className="intro__wrapper">
-        <h1 className="intro__name animated fadeIn"><span>Louis<br />Pham</span></h1>
-        <div className="intro__title animated fadeIn">Front-end developer.</div>
-      </div>
       <div className="intro__heatpipe" aria-hidden="true"></div>
       <div className="intro__moon" aria-hidden="true"></div>
+      <h1 className="intro__name animated fadeIn"><span>Louis<br />Pham</span></h1>
+      <div className="intro__title animated fadeIn">Front-end developer.</div>
     </section>
   );
 }
