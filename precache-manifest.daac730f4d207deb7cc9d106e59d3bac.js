b7dcce347af6271042fc66c9f4f64f24966005b1
louis-pham <l.c.pham@hotmail.com>
2020-01-31 16:51:21 -0500

    Updates

---
+++ precache-manifest.daac730f4d207deb7cc9d106e59d3bac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb2280ce7be53fb53f2e00f3bc60577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "584d87657434d565a10f",
+    "url": "/static/css/main.afe6d72a.chunk.css"
+  },
+  {
+    "revision": "3983f2e3c69934ea7392",
+    "url": "/static/js/2.08c07a4c.chunk.js"
+  },
+  {
+    "revision": "584d87657434d565a10f",
+    "url": "/static/js/main.295d4f6e.chunk.js"
+  },
+  {
+    "revision": "086d9acea5eeeca299bf",
+    "url": "/static/js/runtime~main.5200c24b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4bd1da21d2c9c4e9df644aacb20b6879.js"
+  "/precache-manifest.daac730f4d207deb7cc9d106e59d3bac.js"
 );
 
 self.addEventListener('message', (event) => {
